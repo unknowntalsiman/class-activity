05762ac8e694ba38310cdfe13ddfe317c7b11f21
unknowntalsiman <tuo98558@temple.edu>
2025-01-28 12:05:58 -0500

    Implemented on CLick Listener.

--- app/src/main/java/edu/temple/classactivity/MainActivity.kt
+++ app/src/main/java/edu/temple/classactivity/MainActivity.kt
@@ -1,14 +1,29 @@
 package edu.temple.classactivity
 
 import android.os.Bundle
+import android.view.View.OnClickListener
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var clickMeButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        clickMeButton = findViewById(R.id.button)
+
+        clickMeButton.setOnClickListener {
+            clickMeButton.text = "Button clicked!"
+        }
+
+
     }
+
+
 }
